d4b6f6847055423998b52a53dde301ab360ce359
escarlet200 <114157080+escarlet200@users.noreply.github.com>
2024-04-09 12:18:39 -0300

    ok

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const botoes = document.querySelector(".botao");
 
 for(let i=0;i <botoes.length;i++){
     botoes(i).onclick = function(){
+
+    for(let j=0;j<botoes.length;j++){
+    botoes(j).classlist.remove("ativo");
+    }
+
         botoes(i).classlist.add("ativo");
     }
     console.log();
